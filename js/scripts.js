9ab214cd428553675afd293c19cefdfa05303b22
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-01 15:02:34 -0700

    Add a major styling overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,11 @@ Pizza.prototype.pizzaTotal = function() {
 
   this.pizzaPrice = pizzaBase + this.pizzaToppings.length * 1.5;
   this.pizzaPrice *= this.pizzaSizeModifier;
+  this.pizzaPrice = this.pizzaPrice.toFixed(2);
 }
 
 var pizzaBase = 12;
+var orderTotal = 0;
 
 //Front-end
 $(function() {
@@ -39,7 +41,23 @@ $(function() {
       customPizza.pizzaToppings.push($(this).val());
     })
     customPizza.pizzaTotal();
+    orderTotal += parseFloat(customPizza.pizzaPrice);
+    $("#empty").hide();
     $("#price-output").text(customPizza.pizzaPrice);
+    $("#order-items").append("<li>");
+    customPizza.pizzaToppings.forEach(function(topping) {
+      $("#order-items li").last().append(topping + " ");
+    })
+    $("#order-items li").last().append(": " + customPizza.pizzaPrice + "</li>");
+    $("#price-output").text("Order Total: $" + parseFloat(orderTotal).toFixed(2));
+    $("#order-listing-total").text("Order Total: $" +  parseFloat(orderTotal).toFixed(2));
+
+    $("#clear-button").click(function() {
+      $("#order-items").html("<li id=\"empty\">Empty!</li>");
+      orderTotal = 0;
+      $("#price-output").text("Order Total: $0.00");
+      $("#order-listing-total").text("Order Total: $0.00");
+    });
   });
   // newPizza.pizzaTotal();
 });
