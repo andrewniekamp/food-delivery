626c06a4d75ba7bbc4c7b546baf268d44f49f9cb
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-01 09:27:08 -0700

    Add constructors for anticipated food items

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 //Back-end
+function Pizza() {
+  this.pizzaSize = false;
+  this.pizzaToppings = [];
+  this.pizzaPrice = 0;
+}
 
+function Salad() {
+  this.saladType = false;
+  this.saladPrice = 0;
+}
+
+function Side() {
+  this.sideType = false;
+  this.sidePrice = 0;
+}
+
+function Dessert() {
+  this.dessertType = false;
+  this.dessertPrice = 0;
+}
 
 //Front-end
 $(function() {
