ad061b392c80dbed7b36ef8ada05d27b13bd82a1
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-01 17:04:51 -0700

    Add click events for the premade pizzas and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Back-end
 function Pizza(size) {
   this.pizzaSizeModifier = size;
-  this.pizzaToppings = [];
+  this.pizzaToppings = ["Custom: "];
 }
 
 function Salad() {
@@ -31,35 +31,60 @@ var orderTotal = 0;
 var orderSideBar = false;
 
 //Front-end
+function addPizza(pizza) {
+  orderTotal += parseFloat(pizza.pizzaPrice);
+  $("#empty").hide();
+  $("#price-output").text(pizza.pizzaPrice);
+  $("#order-items").append("<li>");
+  pizza.pizzaToppings.forEach(function(topping) {
+    $("#order-items li").last().append(topping + " ");
+  })
+  $("#order-items li").last().append(" <br>  for: $" + pizza.pizzaPrice + "</li>");
+  $("#price-output").text("Order Total: $" + parseFloat(orderTotal).toFixed(2));
+  $("#order-listing-total").text("Order Total: $" +  parseFloat(orderTotal).toFixed(2));
+
+  $("#clear-button").click(function() {
+    $("#order-items").html("<li id=\"empty\">Empty!</li>");
+    orderTotal = 0;
+    $("#price-output").text("Order Total: $0.00");
+    $("#order-listing-total").text("Order Total: $0.00");
+  });
+}
+
 $(function() {
   //Why can't this submit when selecting form ID?
+  $("#add-alfredo").click(function() {
+    var alfredo = new Pizza();
+    alfredo.pizzaToppings = ["Alfredo Special"]
+    alfredo.pizzaPrice = 18.99;
+    addPizza(alfredo);
+  });
+
+  $("#add-pepperoni").click(function() {
+    var pepperoni = new Pizza();
+    pepperoni.pizzaToppings = ["Classic Pepperoni"]
+    pepperoni.pizzaPrice = 15.52;
+    addPizza(pepperoni);
+  });
+
+  $("#add-supreme").click(function() {
+    var supreme = new Pizza();
+    supreme.pizzaToppings = ["Supreme"]
+    supreme.pizzaPrice = 19.99;
+    addPizza(supreme);
+  });
+
   $("form#topping-form").submit(function(event) {
     event.preventDefault();
-
     var sizeInput = $("#custom-size").val();
     var customPizza = new Pizza(sizeInput);
     $("input:checkbox[name=custom]:checked").each(function() {
       customPizza.pizzaToppings.push($(this).val());
     })
     customPizza.pizzaTotal();
-    orderTotal += parseFloat(customPizza.pizzaPrice);
-    $("#empty").hide();
-    $("#price-output").text(customPizza.pizzaPrice);
-    $("#order-items").append("<li>");
-    customPizza.pizzaToppings.forEach(function(topping) {
-      $("#order-items li").last().append(topping + " ");
-    })
-    $("#order-items li").last().append(" <br>  for: $" + customPizza.pizzaPrice + "</li>");
-    $("#price-output").text("Order Total: $" + parseFloat(orderTotal).toFixed(2));
-    $("#order-listing-total").text("Order Total: $" +  parseFloat(orderTotal).toFixed(2));
-
-    $("#clear-button").click(function() {
-      $("#order-items").html("<li id=\"empty\">Empty!</li>");
-      orderTotal = 0;
-      $("#price-output").text("Order Total: $0.00");
-      $("#order-listing-total").text("Order Total: $0.00");
-    });
+    addPizza(customPizza);
   });
+
   $("#vertical-text").click(function() {
     if (orderSideBar === false) {
       console.log(orderSideBar);
@@ -76,6 +101,5 @@ $(function() {
       orderSideBar = false;
       console.log(orderSideBar);
     }
-  })
-  // newPizza.pizzaTotal();
+  });
 });
